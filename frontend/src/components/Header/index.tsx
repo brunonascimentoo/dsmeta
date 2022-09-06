d7535d5b23af1d56e543096a54b7c44d8262e790
Bruno Nascimento <bnascimentos1996@gmail.com>
2022-09-05 21:43:01 -0300

    outros componentes

---
+++ frontend/src/components/Header/index.tsx
@@ -0,0 +1,19 @@
+import logo from '../../assets/img/logo.svg'
+import './styles.css'
+
+function Header() {
+    return (
+
+        <header>
+            <div className="dsmeta-logo-container">
+                <img src={logo} alt="logo DSMeta" />
+                    <h1>DSMeta</h1>
+                    <p>Desenvolvido por
+                        <a href="https://www.instagram.com/brunonnascimento_/" target="_blank"> @brunonnascimento_ </a>
+                    </p>
+            </div>
+        </header>
+    )
+}
+
+export default Header
