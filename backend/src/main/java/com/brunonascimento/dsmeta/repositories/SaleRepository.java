a9208c05129046a719e7f6d1e703e932cf79643c
Bruno Nascimento <bnascimentos1996@gmail.com>
2022-09-07 15:01:32 -0300

    API teste

---
+++ backend/src/main/java/com/brunonascimento/dsmeta/repositories/SaleRepository.java
@@ -0,0 +1,9 @@
+package com.brunonascimento.dsmeta.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.brunonascimento.dsmeta.entities.Sale;
+
+public interface SaleRepository extends JpaRepository<Sale, Long> {
+
+}
